46cbb75fe254214d39ff1262fdf00afdbb802c6c
Dullguunuu <dulguun.nst@gmail.com>
2022-12-28 12:23:28 +0800

    `

--- Dulguunuu/product list/main.js
+++ Dulguunuu/product list/main.js
@@ -6,6 +6,8 @@ let rating = document.getElementById("rating")
 let price = document.getElementById("price")
 let productData = [];
 let allCategories = [];
+let rowCategories = document.getElementById("rowCat")
+let colCategories = document.getElementById("colCat")
 let products = document.getElementById("products")
 
 
@@ -15,6 +17,7 @@ function data() {
         .then((data) => {
             productData = { ...data }
             console.log(data.products);
+            rowCategory()
             fillProducts()
         })
         .catch((err) => console.log(err));
@@ -22,34 +25,45 @@ function data() {
 data()
 
 
-function printCategory() {
-    productData.map(c =>)
-    if (!allCategories) {
-
-    }
+function rowCategory() {
+    productData.products.map((c) => {
+        if (!allCategories.includes(c.category)) {
+            allCategories.push(c.category);
+            rowCategories.innerHTML += `
+            <div class="list-group">
+                <button type="button" class="list-group-item list-group-item-action" aria-current="true">${c.category}</button>
+            </div>`
+            // colCategories.innerHTML += `
+            // <div class="list-group">
+            //     <button type="button" class="list-group-item list-group-item-action" aria-current="true">${c.category}</button>
+            // </div>`
+        }
+    })
 }
+
+
 
 
 function fillProducts() {
     for (let i = 0; i < productData.products.length; i++) {
         products.innerHTML += `
         <div id="card" class="card" style="width: 18rem;">
-        <div class="ratio ratio-4x3">
-        <img src="${productData.products[i].thumbnail}" id="image">
-        </div>
-            <div class="card-body">
-            <h5 id="title" class="card-title">${productData.products[i].title}</h5>
-            <div>
-            <p id="price">${"$" + productData.products[i].price}</p>
-            <p id="discount">${"$" + productData.products[i].discount}</p>
+            <div class="ratio ratio-4x3">
+                <img src="${productData.products[i].thumbnail}" id="image">
             </div>
-            <p id="description" class="card-text text-truncate">${productData.products[i].description.substr(0, 50)}</p>
-            <div id="" class="d-flex justify-content-between card-body">
-                <p id="rating">${productData.products[i].rating}</p>
-                <i class="bi bi-cart"></i>
-            </div>
-            </div>
-        </div > `
+            <div class="card-body p-3">
+                <h5 id="title" class="card-title">${productData.products[i].title}</h5>
+                <div>
+                    <p id="price">${"$" + productData.products[i].price}</p>
+                    <p id="discount">${"$" + productData.products[i].discount}</p>
+                </div>
+                <p id="description" class="card-text">${productData.products[i].description.substr(0, 60)}...</p >
+                <div class="card-body d-flex justify-content-between p-0">
+                    <p id="rating">${productData.products[i].rating}</p>
+                    <button type="button" class="btn btn-outline-primary"><i class="bi bi-cart" style="font-size: 1.5rem; color: cornflowerblue;"></i></button>
+                </div>
+            </div >
+        </div >`
     }
 }
 
